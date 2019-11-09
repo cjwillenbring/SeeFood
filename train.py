19f64400c6cb0721706fb73aacb4cd9e6dfeef58
Max Conradt <max@tilefive.com>
2019-11-09 17:41:56 -0600

    remove normalization

--- train.py
+++ train.py
@@ -21,14 +21,12 @@ def load_datasets():
         'train': transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.ToTensor()
         ]),
         'val': transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.ToTensor()
         ]),
     }
 
