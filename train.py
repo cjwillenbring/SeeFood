408e1e8135b51d8e08d1e8b3de8755899b025d4e
Max Conradt <max@tilefive.com>
2019-11-09 19:03:22 -0600

    Use pre-trained resnet

--- train.py
+++ train.py
@@ -23,12 +23,16 @@ def load_datasets():
         'train': transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ]),
     }
 
@@ -110,7 +114,7 @@ def run(model, criterion, optimizer, scheduler, loaders, sizes, n_epochs = 25):
 
 
 def load_model():
-    model = torchvision.models.vgg11(pretrained=False, num_classes=NUM_CLASSES)
+    model = torchvision.models.resnet18(pretrained=True)
     model = model.to(device)
     print('DEVICE: ', device)
     return model
