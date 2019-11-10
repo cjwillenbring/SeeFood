420aebd5a7bed3f921f3a5b142c8ee96575fa945
Max Conradt <max@tilefive.com>
2019-11-10 06:40:34 -0600

    Try ResNet 18

--- train.py
+++ train.py
@@ -48,17 +48,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print("device: ", device)
 
-model = models.resnet50(pretrained=True).to(device)
+model = models.resnet18(pretrained=True).to(device)
 
 for param in model.parameters():
     param.requires_grad = False
 
 model.fc = nn.Sequential(
-    nn.Dropout(0.5),
     nn.Linear(2048, 128),
     nn.BatchNorm1d(128),
     nn.ReLU(inplace=True),
-    nn.Dropout(0.4),
     nn.Linear(128, 101)
 ).to(device)
 
