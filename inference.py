3fd00d038d268357201d89d4dc7dd57292d9d4fe
Max Conradt <max@tilefive.com>
2019-11-09 23:30:07 -0600

    Remove weird random crops and shit

--- inference.py
+++ inference.py
@@ -1,5 +1,36 @@
 from train import load_model
+import os
+from torchvision import transforms
+from flask import Flask, request
+from PIL import Image
+from io import BytesIO
+import base64
 
 model = load_model()
 
 model.load_state_dict()
+
+app = Flask(__name__)
+
+
+inference_transforms = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+def image_from_base64(b64):
+    data = {'img': b64}
+    return Image.open(BytesIO(base64.b64decode(data)))
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    img = image_from_base64(request.get_json())
+    tensor = inference_transforms(img)
+    return 'Hello {}!\n'.format(target)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
--- train.py
+++ train.py
@@ -24,15 +24,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def load_datasets():
     data_transforms = {
         'train': transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            transforms.Resize(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
