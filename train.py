39348cf9886c763b5c286ed4887b5af002f4e3e0
Max Conradt <max@tilefive.com>
2019-11-23 19:36:45 -0600

    Decrease scope size in hopes of decreasing memory insanity

--- train.py
+++ train.py
@@ -16,56 +16,58 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device: ", device)
 
 
+def train(network, c, op, loader, dataset):
+    network.train()
+    running_corrects = 0
+    running_loss = 0
+    for inputs, labels in loader:
+        op.zero_grad()
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = network(inputs)
+        loss = c(outputs, labels)
+        loss.backward()
+        op.step()
+        _, preds = torch.max(outputs, 1)
+        running_loss += float(loss) * float(inputs.size(0))
+        # this totally may have been it. If it were batch size it would have failed at any one point in time.
+        running_corrects += torch.sum(preds == labels.data).item()
+    return running_corrects / len(dataset), running_loss / len(dataset)
+
+
+def evaluate(network, c, loader, dataset):
+    network.eval()
+    running_corrects = 0
+    running_loss = 0
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = network(inputs)
+            loss = c(outputs)
+            _, preds = torch.max(outputs, 1)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data).item()
+    return running_corrects / len(dataset), running_loss / len(dataset)
+
+
 def train_model(network, c, op, num_epochs=3):
     train_loader, val_loader, train_set, val_set = loaders(sys.argv[1])
-    image_datasets = {'val': val_set, 'train': train_set}
-    dataloaders = {'val': val_loader, 'train': train_loader}
     best_acc = 0.0
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch+1, num_epochs))
         print('-' * 10)
 
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                network.train()
-            else:
-                network.eval()
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            for inputs, labels in dataloaders[phase]:
-                op.zero_grad()
-                inputs = inputs.to(device)
-
-                outputs = network(inputs)
-                input_size = inputs.size(0)
-                labels = labels.to(device)
-                loss = c(outputs, labels)
-
-                if phase == 'train':
-                    loss.backward()
-                    op.step()
-
-                _, preds = torch.max(outputs, 1)
-                running_loss += float(loss) * float(input_size)
-                # this totally may have been it. If it were batch size it would have failed at any one point in time.
-                running_corrects += torch.sum(preds == labels.data).item()
-
-                del inputs
-                del outputs
-                del labels
-                del loss
-
-            epoch_loss = running_loss / len(image_datasets[phase])
-            epoch_acc = running_corrects / len(image_datasets[phase])
-            profile()
-            if epoch_acc > best_acc and phase == 'val':
-                torch.save(model.state_dict(), 'model.pt')
-
-            print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,
-                                                        epoch_loss,
-                                                        epoch_acc))
+        profile()
+        train_acc, train_loss = train(network, c, op, train_loader, train_set)
+        profile()
+        val_acc, val_loss = evaluate(network, c, train_loader, train_set)
+        profile()
+        if val_acc > best_acc:
+            torch.save(model.state_dict(), 'model.pt')
+            best_acc = val_acc
+        print('Train: {:.4f}, acc: {:.4f}'.format(train_loss, train_acc))
+        print('Val: {:.4f}, acc: {:.4f}'.format(val_loss, val_acc))
     return network
 
 
